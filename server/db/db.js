f161f1e0eb6221bf2fd239445a7d0e2947af3196
juandsierra82 <juandsierra82@gmail.com>
2015-11-06 16:57:07 -0600

    question model added

--- server/db/db.js
+++ server/db/db.js
@@ -4,6 +4,8 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
   var User = require('./userModel/userModel.js')
+  var Score = require('./scoreModel/scoreModel.js')
+  var Questions = require('./questionModel/questionModel.js')
   console.log('connected to the database')
 });
 

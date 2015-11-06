34953369a6f47824c1f87d6e6201a14593fdb76b
juandsierra82 <juandsierra82@gmail.com>
2015-11-06 16:06:39 -0600

    added ionic framework and served static server file

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 var express     = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var db = require('./db/db.js')
 var app = express();
 
-
-	app.get('/', function (req, res) {
-	  res.send('Hello World!');
-	});
+	app.use(morgan('dev'));
 
 
+	app.use(bodyParser.urlencoded({extended: true}));
+
+	app.use('/',express.static(__dirname + '../../quiz/www'));
 
 	mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/quiz');
 
